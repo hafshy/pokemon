822ddf463094f305d08106667afdfe0dd8eddcfe
Hafshy <hafshy@gmail.com>
2023-09-20 12:55:18 +0700

    feat: add infinite scroll with button

--- lib/pokemon_list/ui/pokemon_list_page.dart
+++ lib/pokemon_list/ui/pokemon_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokemon/pokemon_list/bloc/pokemon_list_bloc.dart';
@@ -11,6 +12,19 @@ class PokemonListPage extends StatefulWidget {
 
 class _PokemonListPageState extends State<PokemonListPage> {
   PokemonListBloc pokemonListBloc = PokemonListBloc();
+  RegExp regex = RegExp(r'/(\d+)/$');
+
+  String extractId(String url) {
+    Match? match = regex.firstMatch(url);
+
+    if (match != null) {
+      String pokemonId = match.group(1)!;
+      print(pokemonId);
+      return pokemonId;
+    } else {
+      return "No ID found in the URL";
+    }
+  }
 
   @override
   void initState() {
@@ -29,11 +43,11 @@ class _PokemonListPageState extends State<PokemonListPage> {
       body: BlocConsumer(
         bloc: pokemonListBloc,
         builder: (context, state) {
-          if (pokemonListBloc.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+          // if (pokemonListBloc.isLoading) {
+          //   return const Center(
+          //     child: CircularProgressIndicator(),
+          //   );
+          // }
           return CustomScrollView(
             slivers: [
               const SliverAppBar.large(
@@ -47,11 +61,37 @@ class _PokemonListPageState extends State<PokemonListPage> {
                     )
                   : SliverGrid(
                       delegate: SliverChildBuilderDelegate((context, index) {
-                        return Container(
-                          color: Colors.blue,
-                          child: Center(
-                            child:
-                                Text(pokemonListBloc.pokemonList[index].name),
+                        return Padding(
+                          padding: index % 2 == 0
+                              ? const EdgeInsets.only(left: 12)
+                              : const EdgeInsets.only(right: 12),
+                          child: Card(
+                            child: InkWell(
+                              onTap: () {
+                                print("yuhu");
+                              },
+                              child: Stack(
+                                // crossAxisAlignment: CrossAxisAlignment.start,
+                                // mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Positioned(
+                                      top: 10,
+                                      left: 10,
+                                      child: Text(
+                                        pokemonListBloc.pokemonList[index].name
+                                            .capitalize(),
+                                        style: const TextStyle(fontSize: 20),
+                                        overflow: TextOverflow.ellipsis,
+                                      )),
+                                  Positioned(
+                                      bottom: 10,
+                                      right: 10,
+                                      child: CachedNetworkImage(
+                                          imageUrl:
+                                              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${extractId(pokemonListBloc.pokemonList[index].url)}.png"))
+                                ],
+                              ),
+                            ),
                           ),
                         );
                       }, childCount: pokemonListBloc.pokemonList.length),
@@ -59,7 +99,23 @@ class _PokemonListPageState extends State<PokemonListPage> {
                           const SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 2,
                               crossAxisSpacing: 10,
-                              mainAxisSpacing: 10))
+                              mainAxisSpacing: 10)),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24),
+                  child: Center(
+                    child: pokemonListBloc.isLoading
+                        ? CircularProgressIndicator()
+                        : (state is LoadedPokemons)
+                            ? TextButton(
+                                onPressed: () {
+                                  pokemonListBloc.add(PokemonListFetchEvent());
+                                },
+                                child: Text("Load More"))
+                            : Container(),
+                  ),
+                ),
+              )
             ],
           );
         },
@@ -68,5 +124,11 @@ class _PokemonListPageState extends State<PokemonListPage> {
         buildWhen: (previous, current) => current is! PokemonListActionState,
       ),
     );
+  }
+}
+
+extension StringExtensions on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${substring(1)}";
   }
 }
